9e5b9e89e71ab58ffd4d7fec50a8f223bd045dd6
Anthony Pereira <pereiratony@rocektmail.com>
2022-05-09 13:18:44 -0500

    add color to string

---
+++ mastermind.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: false
+
+class Board
+  attr_accessor :red, :green, :blue, :yelow,
+                :orange, :purple
+
+  def initialize(red, green, blue, yellow, orange, purple)
+    @red = red
+    @green = green
+    @blue = blue
+    @yellow = yellow
+    @orange = orange
+    @purple = purple
+  end
+end
+
+# change color of string
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
